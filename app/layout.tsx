3fdae5a1332de066c24f4109d3af1bba479f7d9f
tejasvi raj <tejasviraj8686@gmail.com>
2023-12-22 11:41:23 +0530

    start background-done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StartBackground";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Space Portfolio',
-  description: 'This is my Portfolio',
-}
+  title: "Space Portfolio",
+  description: "This is my Portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>{children}</body>
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
