ddda355f4308ad8a5e59fd389ff2c7c63f345785
tejasvi raj <tejasviraj8686@gmail.com>
2023-12-22 11:28:42 +0530

    hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
+import Hero from '@/components/main/Hero'
 
 export default function Home() {
   return (
-  <main>space portfolio</main>
+  <main className='h-full w-full '>
+    <div className='flex flex-col h-[850px] gap-20'>
+     <Hero />
+    </div>
+  </main>
   )
 }
